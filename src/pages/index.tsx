d25f6a71224dbf594818f9a60b055b1e1c61a208
Robson Natanael <natanaelrobson@gmail.com>
2022-05-21 18:25:46 -0300

    feat: create banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 
 import { Flex } from '@chakra-ui/react';
 
+import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
   return (
     <Flex direction="column" h="100vh">
       <Header />
+      <Banner />
     </Flex>
   );
 };
