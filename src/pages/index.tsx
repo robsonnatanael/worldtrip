fcfc25b24cd9587d3e36f3c5f9a34b554174ddac
Robson Natanael <natanaelrobson@gmail.com>
2022-05-21 13:25:00 -0300

    chore: configure prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1>WorldTrip</h1>
-  )
-}
+  return <h1>WorldTrip</h1>;
+};
 
-export default Home
+export default Home;
