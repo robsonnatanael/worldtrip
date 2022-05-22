de06c0b9f40637a8c66686846f95586224bf697a
Robson Natanael <natanaelrobson@gmail.com>
2022-05-22 11:41:12 -0300

    feat: create continent banner

--- src/pages/continent/index.tsx
+++ src/pages/continent/index.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from 'next';
 
+import { ContinentBanner } from '../../components/ContinentBanner';
 import { Header } from '../../components/Header';
 
 const Continent: NextPage = () => {
   return (
     <>
       <Header />
+      <ContinentBanner />
     </>
   );
 };
